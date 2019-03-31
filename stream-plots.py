8bdaf86636ff669dd146d4bf9a1bf2aa041cd3b5
jme2103 <43256224+jme2103@users.noreply.github.com>
2019-03-31 14:20:02 -0500

    custom streamlines
    Added the ability to specify the starting points for the streamlines.

--- stream-plots.py
+++ stream-plots.py
@@ -1,10 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# the points for generating the velocity field
 y,x = np.mgrid[-3:3:100j,-3:3:100j]
+
+# the initial conditions for generating stream lines
+# provided as an array of points in the plane
+xcoords = np.linspace(-3,3,22,endpoint=True)
+ycoords = np.zeros_like(xcoords)
+strmpts = np.array(zip(xcoords, ycoords))
 
 u = 0.5*y
 v = -8*x
 
-plt.streamplot(x,y,u,v,density=1)
+plt.streamplot(x, y, u, v, start_points=strmpts, density=35)
 plt.show()
